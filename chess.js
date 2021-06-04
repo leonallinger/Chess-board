112dddd317b9705ef9ab9962bdf6e8c384497fd2
leonallinger <leon.allinger@gmail.com>
2021-06-04 02:22:46 +0200

    Add files via upload

---
+++ chess.js
@@ -0,0 +1,34 @@
+import {renderPieces} from './renderPieces.service.js'
+addEventListener('DOMContentLoaded', _ =>{
+    renderPieces.piecesRender()
+})
+
+let board = document.getElementById("chessBoard");
+const renderBoard = function() {
+    let change = false;
+    let backgroundColor = 'BurlyWood';
+    for (let i = 0; i < 64; i++) {
+        let n = i+1;
+        let square = document.createElement('DIV');
+        square.className = 'square';
+        square.id = 'a'+n;
+        if (i % 8 === 0 || i === 0)
+        {
+            change = true;
+        }
+
+        if (!change) {
+            if (backgroundColor === 'SaddleBrown'){
+                backgroundColor = 'BurlyWood';
+            }
+            else{
+                backgroundColor = 'SaddleBrown';
+            }
+        }
+
+        square.style.backgroundColor = backgroundColor;
+        board.appendChild(square);
+        change = false;
+    }
+}
+renderBoard();
---
+++ renderPieces.service.js
@@ -0,0 +1,29 @@
+import {$, $$, $$$} from './utilities.js'
+import {initialGame} from './initialGame.config.js'
+import {potentialGame} from './potentialGame.config.js'
+import {chessConfig} from './chessConfig.config.js'
+import {pieceImages} from './pieceImages.config.js'
+
+export const renderPieces = {
+
+	piecesRender() {
+		
+		const gameSetup = initialGame;
+
+		this.placePieces(gameSetup);
+	},
+
+	placePieces(gameSetup){
+		for (const piecePosition in gameSetup){
+			const pieceType = gameSetup[piecePosition];
+			const pieceImageLocation = pieceImages[pieceType];
+
+			const imgElement = document.createElement('img');
+			imgElement.classList.add('piece');
+			imgElement.setAttribute('piece-type', pieceType);
+			imgElement.src = `${pieceImageLocation}`;
+
+			$(`#${piecePosition}`).append(imgElement);
+		}
+	},
+}

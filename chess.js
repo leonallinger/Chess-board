2d9f92c6a1b75d99d8dba3bb6f4f8f56659cffb1
leonallinger <leon.allinger@gmail.com>
2021-06-08 01:28:16 +0200

    Update chess.js

--- chess.js
+++ chess.js
@@ -10,8 +10,17 @@ window.allowDrop = allowDrop;
 
 function drop(ev) {
     ev.preventDefault();
+
     let data = ev.dataTransfer.getData("piece");
-    ev.target.appendChild(document.getElementById(data));
+    let s = document.getElementById(data);
+
+    if (ev.target.parentNode.id === "chessBoard") {
+        ev.target.appendChild(s);
+    }
+    else {
+        ev.target.parentNode.appendChild(s);
+        ev.target.parentNode.removeChild(ev.target);
+    }
 }
 window.drop = drop;
 

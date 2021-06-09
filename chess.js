156054c5c0da4125e45bc4d79a8c7b36b704ee07
leonallinger <leon.allinger@gmail.com>
2021-06-10 01:27:52 +0200

    Update chess.js

--- chess.js
+++ chess.js
@@ -10,6 +10,8 @@ addEventListener('DOMContentLoaded', _ => {
 
 import { knightMoves } from './pieceMoves.service.js'
 
+import { dropPieces } from './dropPieces.service.js'
+
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -22,67 +24,21 @@ function drag(ev) {
 }
 window.drag = drag;
 
-let playerTurn = "white";
 function drop(ev) {
     ev.preventDefault();
 
-    let data = ev.dataTransfer.getData("piece");
+    let startPiece = ev.dataTransfer.getData("piece");
     let startSquare = ev.dataTransfer.getData("startSquare");
-    let s = document.getElementById(data);
-
-    let targetPieceColor = ev.target.id.substring(0, 5);
-    let startingPieceColor = data.substring(0, 5);
-    let startingPieceType = data.substring(6, 8)
+    
+    let startingPieceType = startPiece.substring(6, 8);
 
     if (startingPieceType === "kn") {
         if (knightMoves(startSquare).indexOf(ev.target.id) > -1 || knightMoves(startSquare).indexOf(ev.target.parentNode.id) > -1 ) {
-            if (ev.target.className === "square") {
-                if (startingPieceColor === "white" && playerTurn === "white") {
-                    ev.target.appendChild(s);
-                    playerTurn = "black";
-                }
-                else if (startingPieceColor === "black" && playerTurn === "black") {
-                    ev.target.appendChild(s);
-                    playerTurn = "white";
-                }
-            }
-            else {
-                if (targetPieceColor === "black" && startingPieceColor === "white" && playerTurn === "white") {
-                    ev.target.parentNode.appendChild(s);
-                    ev.target.parentNode.removeChild(ev.target);
-                    playerTurn = "black";
-                }
-                else if (targetPieceColor === "white" && startingPieceColor === "black" && playerTurn === "black") {
-                    ev.target.parentNode.appendChild(s);
-                    ev.target.parentNode.removeChild(ev.target);
-                    playerTurn = "white";
-                }
-            }
+            dropPieces(ev);
         }
     }
     else {
-        if (ev.target.className === "square") {
-            if (startingPieceColor === "white" && playerTurn === "white") {
-                ev.target.appendChild(s);
-                playerTurn = "black";
-            }
-            else if (startingPieceColor === "black" && playerTurn === "black") {
-                ev.target.appendChild(s);
-                playerTurn = "white";
-            }
-        }
-        else {
-            if (targetPieceColor === "black" && startingPieceColor === "white" && playerTurn === "white") {
-                ev.target.parentNode.appendChild(s);
-                ev.target.parentNode.removeChild(ev.target);
-                playerTurn = "black";
-            }
-            else if (targetPieceColor === "white" && startingPieceColor === "black" && playerTurn === "black") {
-                ev.target.parentNode.appendChild(s);
-                ev.target.parentNode.removeChild(ev.target);
-                playerTurn = "white";
-            }
-        }
+        dropPieces(ev);
     }
     
 }

0f0ba24eb161e07754add5df65522d1db36ca9ef
leonallinger <leon.allinger@gmail.com>
2021-09-08 12:17:10 +0200

    Update renderPieces.service.js

--- renderPieces.service.js
+++ renderPieces.service.js
@@ -1,29 +1,26 @@
 import { initialGame } from './initialGame.config.js'
 import { pieceImages } from './pieceImages.config.js'
 
-export const renderPieces = {
+export const piecesRender = function() {
 
-	piecesRender() {
-		
-		const gameSetup = initialGame;
+	const gameSetup = initialGame;
 
-		this.placePieces(gameSetup);
-	},
+	placePieces(gameSetup);
+}
 
-	placePieces(gameSetup){
-		for (const piecePosition in gameSetup) {
-			const pieceType = gameSetup[piecePosition];
+const placePieces = function(gameSetup){
+	for (const piecePosition in gameSetup) {
+		const pieceType = gameSetup[piecePosition];
 
-			const imgElement = document.createElement('img');
-			imgElement.classList.add('piece');
-			imgElement.setAttribute('id', pieceType + '_' + piecePosition);
+		const imgElement = document.createElement('img');
+		imgElement.classList.add('piece');
+		imgElement.setAttribute('id', pieceType + '_' + piecePosition);
 
-			imgElement.setAttribute('draggable', 'true');
-			imgElement.setAttribute('ondragstart', 'drag(event)')
-			imgElement.src = pieceImages[pieceType];
+		imgElement.setAttribute('draggable', 'true');
+		imgElement.setAttribute('ondragstart', 'drag(event)');
+		imgElement.src = pieceImages[pieceType];
 
-			const square = document.getElementById(piecePosition);
-			square.append(imgElement);
-		}
-	},
+		const square = document.getElementById(piecePosition);
+		square.append(imgElement);
+	}
 }

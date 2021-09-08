236f98a3428c5344d149f7f062d37cd802b6f108
leonallinger <leon.allinger@gmail.com>
2021-09-08 12:16:42 +0200

    Update renderBoard.service.js

--- renderBoard.service.js
+++ renderBoard.service.js
@@ -2,41 +2,41 @@ function nextChar(letter, amount) {
     return String.fromCharCode(letter.charCodeAt(0) + amount);
 }
 
-export const renderBoard = {
-    boardRender() {
-        const board = document.getElementById("chessBoard");
-        let change = true;
-        let backgroundColor = 'BurlyWood';
-        let file = 'a';
-        let rank = '8';
-        for (let i = 0; i < 64; i++) {
-            let square = document.createElement('DIV');
-            square.className = 'square';
-            square.setAttribute('ondragover', 'allowDrop(event)')
-            square.setAttribute('ondrop', 'drop(event)')
-            square.id = file + rank;
-            file = nextChar(file, 1);
-            if (file === 'i') {
-                file = 'a';
-                rank--;
-            }
-
-            if (i % 8 === 0 || i === 0) {
-                change = false;
-            }
-
-            if (change) {
-                if (backgroundColor === 'SaddleBrown') {
-                    backgroundColor = 'BurlyWood';
-                }
-                else {
-                    backgroundColor = 'SaddleBrown';
-                }
-            }
-
-            square.style.backgroundColor = backgroundColor;
-            board.appendChild(square);
-            change = true;
+export const boardRender = function() {
+    const board = document.getElementById("chessBoard");
+    let change = true;
+    let backgroundColor = 'BurlyWood';
+    let file = 'a';
+    let rank = '8';
+    for (let i = 0; i < 64; i++) {
+        let square = document.createElement('DIV');
+        square.className = 'square';
+        square.setAttribute('ondragover', 'allowDrop(event)');
+        square.setAttribute('ondrop', 'drop(event)');
+        square.setAttribute('isAttackedByWhite', 'false');
+        square.setAttribute('isAttackedByBlack', 'false');
+        square.id = file + rank;
+        file = nextChar(file, 1);
+        if (file === 'i') {
+            file = 'a';
+            rank--;
         }
+
+        if (i % 8 === 0 || i === 0) {
+            change = false;
+        }
+
+        if (change) {
+            if (backgroundColor === 'SaddleBrown') {
+                backgroundColor = 'BurlyWood';
+            }
+            else {
+                backgroundColor = 'SaddleBrown';
+            }
+        }
+
+        square.style.backgroundColor = backgroundColor;
+        board.appendChild(square);
+        change = true;
     }
 }

4ebbb84e73a095d30a4e9e4de2d489ee0178a327
leonallinger <leon.allinger@gmail.com>
2021-09-08 12:16:11 +0200

    Update pieceMoves.service.js

--- pieceMoves.service.js
+++ pieceMoves.service.js
@@ -2,21 +2,19 @@ function nextChar(letter, amount) {
     return String.fromCharCode(letter.charCodeAt(0) + amount);
 }
 
-export const pawnMoves = function(square, ev) {
-    let data = ev.dataTransfer.getData("piece");
-    let startingPieceColor = data.substring(0, 5);
+export const pawnMoves = function(square, pieceColor) {
 
     let file = square.substring(0, 1);
     let rank = parseInt(square.substring(1, 2));
 
-    let moves = new Array(4);
+    let moves = new Array();
 
     var p, q;
-    if (startingPieceColor === "white"){
+    if (pieceColor === "white"){
         p = 1;
         q = 2;
     }
-    else if (startingPieceColor === "black"){
+    else if (pieceColor === "black"){
         p = -1;
         q = 7;
     }
@@ -33,19 +31,33 @@ export const pawnMoves = function(square, ev) {
         }
     }
 
-    let pawnCaptures1 = nextChar(file, 1) + (rank + 1*p);
-    let pawnCaptures2 = nextChar(file, -1) + (rank + 1*p);
+    return moves;
+}
 
-    if (document.getElementById(pawnCaptures1) != null){
-        if (document.getElementById(pawnCaptures1).children.length > 0) {
-            moves[2] = pawnCaptures1;
-        }
+export const pawnCaptures = function (square, pieceColor) {
+
+    let file = square.substring(0, 1);
+    let rank = parseInt(square.substring(1, 2));
+
+    let moves = new Array();
+
+    var p;
+    if (pieceColor === "white") {
+        p = 1;
     }
-        
-    if (document.getElementById(pawnCaptures2) != null){
-        if (document.getElementById(pawnCaptures2).children.length > 0) {
-            moves[3] = pawnCaptures2;
-        }
+    else if (pieceColor === "black") {
+        p = -1;
+    }
+
+    let pawnCaptures1 = nextChar(file, 1) + (rank + 1 * p);
+    let pawnCaptures2 = nextChar(file, -1) + (rank + 1 * p);
+
+    if (document.getElementById(pawnCaptures1) != null) {
+        moves[0] = pawnCaptures1;
+    }
+
+    if (document.getElementById(pawnCaptures2) != null) {
+        moves[1] = pawnCaptures2;
     }
 
     return moves;
@@ -55,19 +67,22 @@ export const kingMoves = function(square) {
     let file = square.substring(0, 1);
     let rank = parseInt(square.substring(1, 2));
 
-    let moves = new Array(8);
-    moves[0] = file + (rank + 1);
-    moves[1] = file + (rank - 1);
-    moves[2] = nextChar(file, 1) + (rank + 1);
-    moves[3] = nextChar(file, 1) + (rank - 1);
-    moves[4] = nextChar(file, -1) + (rank + 1);
-    moves[5] = nextChar(file, -1) + (rank - 1);
-    moves[6] = nextChar(file, 1) + rank;
-    moves[7] = nextChar(file, -1) + rank;
+    let potentialMoves = new Array(8);
+    potentialMoves[0] = file + (rank + 1);
+    potentialMoves[1] = file + (rank - 1);
+    potentialMoves[2] = nextChar(file, 1) + (rank + 1);
+    potentialMoves[3] = nextChar(file, 1) + (rank - 1);
+    potentialMoves[4] = nextChar(file, -1) + (rank + 1);
+    potentialMoves[5] = nextChar(file, -1) + (rank - 1);
+    potentialMoves[6] = nextChar(file, 1) + rank;
+    potentialMoves[7] = nextChar(file, -1) + rank;
 
-    for (let i = 0; i < 8; i++){
-        if (document.getElementById(moves[i]) === null){
-            moves.splice(i, 1);
+    let moves = new Array();
+    let j = 0;
+    for (let i = 0; i < 8; i++) {
+        if (document.getElementById(potentialMoves[i]) != null) {
+            moves[j] = potentialMoves[i];
+            j++;
         }
     }
 
@@ -78,19 +93,22 @@ export const knightMoves = function(square) {
     let file = square.substring(0, 1);
     let rank = parseInt(square.substring(1, 2));
 
-    let moves = new Array(8);
-    moves[0] = nextChar(file, -2) + (rank + 1);
-    moves[1] = nextChar(file, -2) + (rank - 1);
-    moves[2] = nextChar(file, -1) + (rank + 2);
-    moves[3] = nextChar(file, -1) + (rank - 2);
-    moves[4] = nextChar(file, 1) + (rank + 2);
-    moves[5] = nextChar(file, 1) + (rank - 2);
-    moves[6] = nextChar(file, 2) + (rank + 1);
-    moves[7] = nextChar(file, 2) + (rank - 1);
+    let potentialMoves = new Array(8);
+    potentialMoves[0] = nextChar(file, -2) + (rank + 1);
+    potentialMoves[1] = nextChar(file, -2) + (rank - 1);
+    potentialMoves[2] = nextChar(file, -1) + (rank + 2);
+    potentialMoves[3] = nextChar(file, -1) + (rank - 2);
+    potentialMoves[4] = nextChar(file, 1) + (rank + 2);
+    potentialMoves[5] = nextChar(file, 1) + (rank - 2);
+    potentialMoves[6] = nextChar(file, 2) + (rank + 1);
+    potentialMoves[7] = nextChar(file, 2) + (rank - 1);
 
+    let moves = new Array();
+    let j = 0;
     for (let i = 0; i < 8; i++){
-        if (document.getElementById(moves[i]) === null){
-            moves.splice(i, 1);
+        if (document.getElementById(potentialMoves[i]) != null) {
+            moves[j] = potentialMoves[i];
+            j++;
         }
     }
 
@@ -101,7 +119,7 @@ export const bishopMoves = function(square) {
     let file = square.substring(0, 1);
     let rank = parseInt(square.substring(1, 2));
 
-    let moves = new Array(32);
+    let moves = new Array();
 
     let i = 0;
 
@@ -132,7 +150,7 @@ export const rookMoves = function(square) {
     let file = square.substring(0, 1);
     let rank = parseInt(square.substring(1, 2));
 
-    let moves = new Array(32);
+    let moves = new Array();
 
     let i = 0;
 

f690ecaade8613066897d09f581923bd19b815d1
leonallinger <leon.allinger@gmail.com>
2021-10-09 21:31:35 +0200

    Update renderPieces.service.js

--- renderPieces.service.js
+++ renderPieces.service.js
@@ -1,30 +1,7 @@
 import { initialGame } from './initialGame.config.js'
 import { pieceImages } from './pieceImages.config.js'
 
-const startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-export const loadPositionFromFen = function(fen){
-
-	let fenBoard = fen.split(" ")[0];
-	let file = 1;
-	let rank = 8;
-
-	for (let symbol of fenBoard){
-		if (symbol === '/'){
-			file = 1;
-			rank--;
-		}
-		else{
-			if (!isNaN(parseInt(symbol))){
-				file += parseInt(symbol);
-			}
-			else{
-				let pieceColor = (symbol === symbol.toUpperCase()) ? "white" : "black";
-			}
-		}
-	}
-}
-
-export const placePieces = function(){
+export const placePieces = function () {
 	const gameSetup = initialGame;
 
 	for (const piecePosition in gameSetup) {

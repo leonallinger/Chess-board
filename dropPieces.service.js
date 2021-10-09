32f2b70345aee6cea6c9863065b38ede49b91c26
leonallinger <leon.allinger@gmail.com>
2021-10-09 21:30:16 +0200

    Update dropPieces.service.js

--- dropPieces.service.js
+++ dropPieces.service.js
@@ -1,85 +1,34 @@
 import { attackedByWhite, attackedByBlack } from './attackedSquares.service.js'
-import { whiteIsInCheck, blackIsInCheck } from './isInCheck.service.js'
 
 let playerTurn = "white";
 
 export const dropPieces = function(ev){
     let data = ev.dataTransfer.getData("piece");
-    let s = document.getElementById(data);
-
-    let startSquare = document.getElementById(ev.dataTransfer.getData("startSquare"));
-
-    let targetPieceColor = ev.target.id.substring(0, 5);
+    let piece = document.getElementById(data);
     let startingPieceColor = data.substring(0, 5);
 
 	if (ev.target.className === "square") {
         if (startingPieceColor === playerTurn) {
 
-            ev.target.appendChild(s);
+            ev.target.appendChild(piece);
+
             playerTurn = (playerTurn === "white") ? "black":"white";
 
             attackedByWhite();
             attackedByBlack();
-
-            switch (startingPieceColor){
-                case "white":
-                    if (whiteIsInCheck() === true){
-                        ev.target.removeChild(s);
-                        startSquare.appendChild(s);
-
-                        attackedByWhite();
-                        playerTurn = "white";
-                    }
-                    break;
-
-                case "black":
-                    if (blackIsInCheck() === true){
-                        ev.target.removeChild(s);
-                        startSquare.appendChild(s);
-
-                        attackedByBlack();
-                        playerTurn = "black";
-                    }
-                    break;
-            }
         }
     }
-
     else {
-        if (targetPieceColor != startingPieceColor && startingPieceColor === playerTurn) {
+        if (startingPieceColor === playerTurn) {
 
             let targetSquare = ev.target.parentNode;
-            targetSquare.appendChild(s);
+            targetSquare.appendChild(piece);
             targetSquare.removeChild(ev.target);
 
             playerTurn = (playerTurn === "white") ? "black":"white";
 
             attackedByWhite();
             attackedByBlack();
-
-            switch (startingPieceColor){
-                case "white":
-                    if (whiteIsInCheck() === true){
-                        targetSquare.removeChild(s);
-                        targetSquare.appendChild(ev.target);
-                        startSquare.appendChild(s);
-
-                        attackedByWhite();
-                        playerTurn = "white";
-                    }
-                    break;
-
-                case "black":
-                    if (blackIsInCheck() === true){
-                        targetSquare.removeChild(s);
-                        targetSquare.appendChild(ev.target);
-                        startSquare.appendChild(s);
-
-                        attackedByBlack();
-                        playerTurn = "black";
-                    }
-                    break;
-            }
         }
     }
 

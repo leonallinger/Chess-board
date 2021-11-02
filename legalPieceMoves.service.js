42468eec2a25c500d0b5d20eff095d9acb02c6ad
leonallinger <leon.allinger@gmail.com>
2021-11-02 23:18:49 +0100

    Update legalPieceMoves.service.js

--- legalPieceMoves.service.js
+++ legalPieceMoves.service.js
@@ -1,13 +1,34 @@
 import { allBlackMoves, allWhiteMoves } from './allMoves.service.js'
 import { attackedByWhite, attackedByBlack } from './attackedSquares.service.js'
 import { whiteIsInCheck, blackIsInCheck } from './isInCheck.service.js'
+import { kingsideCastle, queensideCastle } from './pieceMoves.service.js'
 
 export const legalWhiteMoves = function () {
     let moves = allWhiteMoves();
+    moves = moves.concat(kingsideCastle("white"));
+    moves = moves.concat(queensideCastle("white"));
 
     let legalMoves = new Array();
 
     for (let i = 0; i < moves.length; i++) {
+        
+        if (moves[i] === "O-O"){
+            if (whiteIsInCheck() === false){
+                if (document.getElementById("f1").getAttribute("isAttackedByBlack") === "false" && document.getElementById("g1").getAttribute("isAttackedByBlack") === "false"){
+                    legalMoves.push(moves[i]);
+                }
+            }
+            continue;
+        }
+        if (moves[i] === "O-O-O"){
+            if (whiteIsInCheck() === false){
+                if (document.getElementById("d1").getAttribute("isAttackedByBlack") === "false" && document.getElementById("c1").getAttribute("isAttackedByBlack") === "false"){
+                    legalMoves.push(moves[i]);
+                }
+            }
+            continue;
+        }
+
         let startSquare = document.getElementById(moves[i].substring(moves[i].length - 5, moves[i].length - 3));
         let startPiece = startSquare.childNodes[0];
 
@@ -50,10 +71,30 @@ export const legalWhiteMoves = function () {
 
 export const legalBlackMoves = function () {
     let moves = allBlackMoves();
+    moves = moves.concat(kingsideCastle("black"));
+    moves = moves.concat(queensideCastle("black"));
 
     let legalMoves = new Array();
 
     for (let i = 0; i < moves.length; i++) {
+
+        if (moves[i] === "O-O"){
+            if (blackIsInCheck() === false){
+                if (document.getElementById("f8").getAttribute("isAttackedByWhite") === "false" && document.getElementById("g8").getAttribute("isAttackedByWhite") === "false"){
+                    legalMoves.push(moves[i]);
+                }
+            }
+            continue;
+        }
+        if (moves[i] === "O-O-O"){
+            if (blackIsInCheck() === false){
+                if (document.getElementById("d8").getAttribute("isAttackedByWhite") === "false" && document.getElementById("c8").getAttribute("isAttackedByWhite") === "false"){
+                    legalMoves.push(moves[i]);
+                }
+            }
+            continue;
+        }
+
         let startSquare = document.getElementById(moves[i].substring(moves[i].length - 5, moves[i].length - 3));
         let startPiece = startSquare.childNodes[0];
 

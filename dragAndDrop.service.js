73a317aed539e72c4b4dd10c3cecd64d7a528c67
leonallinger <leon.allinger@gmail.com>
2021-11-21 01:38:56 +0100

    Update dragAndDrop.service.js

--- dragAndDrop.service.js
+++ dragAndDrop.service.js
@@ -2,6 +2,13 @@ import { legalWhiteMoves, legalBlackMoves } from './legalPieceMoves.service.js'
 import { dropPieces } from './dropPieces.service.js'
 
 let playerTurn = "white";
+let whiteMoves;
+let blackMoves;
+
+export const initialLegalMoves = function () {
+    whiteMoves = legalWhiteMoves();
+    blackMoves = legalBlackMoves();
+}
 
 export const allowDrop = function(ev) {
     ev.preventDefault();
@@ -19,6 +26,7 @@ export const drop = function (ev) {
 
     let startPiece = ev.dataTransfer.getData("piece");
     let startSquare = ev.dataTransfer.getData("startSquare");
+    let targetSquare = (ev.target.className === "square") ? ev.target.id : ev.target.parentNode.id;
 
     let data = ev.dataTransfer.getData("piece");
     let startingPieceColor = data.substring(0, 5);
@@ -30,76 +38,84 @@ export const drop = function (ev) {
         return;
     }
 
-    let whiteMoves = legalWhiteMoves();
-    let blackMoves = legalBlackMoves();
-
     if (startingPieceType === "pawn") {
-        if (whiteMoves.indexOf(startSquare + '-' + ev.target.id) > -1 || whiteMoves.indexOf(startSquare + 'x' + ev.target.parentNode.id) > -1 || blackMoves.indexOf(startSquare + '-' + ev.target.id) > -1 || blackMoves.indexOf(startSquare + 'x' + ev.target.parentNode.id) > -1) {
-            dropPieces(startSquare, ev.target.id, startingPieceColor);
+        if (whiteMoves.indexOf(startSquare + '-' + targetSquare) > -1 || whiteMoves.indexOf(startSquare + 'x' + targetSquare) > -1 || blackMoves.indexOf(startSquare + '-' + targetSquare) > -1 || blackMoves.indexOf(startSquare + 'x' + targetSquare) > -1) {
+            dropPieces(startSquare, targetSquare, startSquare + '-' + targetSquare);
             playerTurn = (playerTurn === "white") ? "black" : "white";
         }
     }
+
     else if (startingPieceType === "king") {
-        if (whiteMoves.indexOf('K' + startSquare + '-' + ev.target.id) > -1 || blackMoves.indexOf('K' + startSquare + '-' + ev.target.id) > -1 || whiteMoves.indexOf('K' + startSquare + '-' + ev.target.parentNode.id) > -1 || blackMoves.indexOf('K' + startSquare + '-' + ev.target.parentNode.id) > -1) {
-            dropPieces(startSquare, ev.target.id, startingPieceColor);
+        if (whiteMoves.indexOf('K' + startSquare + '-' + targetSquare) > -1 || blackMoves.indexOf('K' + startSquare + '-' + targetSquare) > -1) {
+            dropPieces(startSquare, targetSquare, 'K' + startSquare + '-' + targetSquare);
             playerTurn = (playerTurn === "white") ? "black" : "white";
         }
-        else if (startingPieceColor === "white"){
-            if (whiteMoves.indexOf("O-O") > -1 && ev.target.id === "g1"){
-                dropPieces(startSquare, ev.target.id, startingPieceColor);
-                dropPieces("h1", "f1", startingPieceColor);
+        else if (startingPieceColor === "white") {
+            if (whiteMoves.indexOf("O-O") > -1 && targetSquare === "g1") {
+                dropPieces(startSquare, targetSquare, "O-O");
+                dropPieces("h1", "f1", "O-O");
                 playerTurn = (playerTurn === "white") ? "black" : "white";
             }
-            else if (whiteMoves.indexOf("O-O-O") > -1 && ev.target.id === "c1"){
-                dropPieces(startSquare, ev.target.id, startingPieceColor);
-                dropPieces("a1", "d1", startingPieceColor);
-                playerTurn = (playerTurn === "white") ? "black" : "white";
-            }
-        }
-        else if (startingPieceColor === "black"){
-            if (blackMoves.indexOf("O-O") > -1 && ev.target.id === "g8"){
-                dropPieces(startSquare, ev.target.id, startingPieceColor);
-                dropPieces("h8", "f8", startingPieceColor);
-                playerTurn = (playerTurn === "white") ? "black" : "white";
-            }
-            else if (blackMoves.indexOf("O-O-O") > -1 && ev.target.id === "b8"){
-                dropPieces(startSquare, ev.target.id, startingPieceColor);
-                dropPieces("a8", "d8", startingPieceColor);
+            else if (whiteMoves.indexOf("O-O-O") > -1 && targetSquare === "c1") {
+                dropPieces(startSquare, targetSquare, "O-O-O");
+                dropPieces("a1", "d1", "O-O-O");
                 playerTurn = (playerTurn === "white") ? "black" : "white";
             }
         }
+        else if (startingPieceColor === "black") {
+            if (blackMoves.indexOf("O-O") > -1 && targetSquare === "g8") {
+                dropPieces(startSquare, targetSquare, "O-O");
+                dropPieces("h8", "f8", "O-O");
+                playerTurn = (playerTurn === "white") ? "black" : "white";
+            }
+            else if (blackMoves.indexOf("O-O-O") > -1 && targetSquare === "c8"){
+                dropPieces(startSquare, targetSquare, "O-O-O");
+                dropPieces("a8", "d8", "O-O-O");
+                playerTurn = (playerTurn === "white") ? "black" : "white";
+            }
+        }
+
     }
     else if (startingPieceType === "knight") {
-        if (whiteMoves.indexOf('N' + startSquare + '-' + ev.target.id) > -1 || blackMoves.indexOf('N' + startSquare + '-' + ev.target.id) > -1 || whiteMoves.indexOf('N' + startSquare + '-' + ev.target.parentNode.id) > -1 || blackMoves.indexOf('N' + startSquare + '-' + ev.target.parentNode.id) > -1) {
-            dropPieces(startSquare, ev.target.id, startingPieceColor);
+        if (whiteMoves.indexOf('N' + startSquare + '-' + targetSquare) > -1 || blackMoves.indexOf('N' + startSquare + '-' + targetSquare) > -1) {
+            dropPieces(startSquare, targetSquare, 'N' + startSquare + '-' + targetSquare);
             playerTurn = (playerTurn === "white") ? "black" : "white";
         }
     }
+
     else if (startingPieceType === "bishop") {
-        if (whiteMoves.indexOf('B' + startSquare + '-' + ev.target.id) > -1 || blackMoves.indexOf('B' + startSquare + '-' + ev.target.id) > -1 || whiteMoves.indexOf('B' + startSquare + '-' + ev.target.parentNode.id) > -1 || blackMoves.indexOf('B' + startSquare + '-' + ev.target.parentNode.id) > -1) {
-            dropPieces(startSquare, ev.target.id, startingPieceColor);
+        if (whiteMoves.indexOf('B' + startSquare + '-' + targetSquare) > -1 || blackMoves.indexOf('B' + startSquare + '-' + targetSquare) > -1) {
+            dropPieces(startSquare, targetSquare, 'B' + startSquare + '-' + targetSquare);
             playerTurn = (playerTurn === "white") ? "black" : "white";
         }
     }
+
     else if (startingPieceType === "rook") {
-        if (whiteMoves.indexOf('R' + startSquare + '-' + ev.target.id) > -1 || blackMoves.indexOf('R' + startSquare + '-' + ev.target.id) > -1 || whiteMoves.indexOf('R' + startSquare + '-' + ev.target.parentNode.id) > -1 || blackMoves.indexOf('R' + startSquare + '-' + ev.target.parentNode.id) > -1) {
-            dropPieces(startSquare, ev.target.id, startingPieceColor);
+        if (whiteMoves.indexOf('R' + startSquare + '-' + targetSquare) > -1 || blackMoves.indexOf('R' + startSquare + '-' + targetSquare) > -1) {
+            dropPieces(startSquare, targetSquare, 'R' + startSquare + '-' + targetSquare);
             playerTurn = (playerTurn === "white") ? "black" : "white";
         }
     }
+
     else if (startingPieceType === "queen") {
-        if (whiteMoves.indexOf('Q' + startSquare + '-' + ev.target.id) > -1 || blackMoves.indexOf('Q' + startSquare + '-' + ev.target.id) > -1 || whiteMoves.indexOf('Q' + startSquare + '-' + ev.target.parentNode.id) > -1 || blackMoves.indexOf('Q' + startSquare + '-' + ev.target.parentNode.id) > -1) {
-            dropPieces(startSquare, ev.target.id, startingPieceColor);
+        if (whiteMoves.indexOf('Q' + startSquare + '-' + targetSquare) > -1 || blackMoves.indexOf('Q' + startSquare + '-' + targetSquare) > -1) {
+            dropPieces(startSquare, targetSquare, 'Q' + startSquare + '-' + targetSquare);
             playerTurn = (playerTurn === "white") ? "black" : "white";
         }
     }
 
     let playerTurnText = document.getElementById("playerTurn");
+    let playerTurnColor = document.getElementById("moveColor");
     if (playerTurn === "white"){
         playerTurnText.innerText = "White to move";
+        playerTurnColor.style.backgroundColor = "white";
     }
     else if (playerTurn === "black"){
         playerTurnText.innerText = "Black to move";
+        playerTurnColor.style.backgroundColor = "black";
     }
+
+    whiteMoves = legalWhiteMoves();
+    blackMoves = legalBlackMoves();
 }
 window.drop = drop;

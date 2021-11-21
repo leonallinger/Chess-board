ac14e744c661d7fe01c71fb8d60e2ebd46471b12
leonallinger <leon.allinger@gmail.com>
2021-11-21 01:39:20 +0100

    Update dropPieces.service.js

--- dropPieces.service.js
+++ dropPieces.service.js
@@ -1,8 +1,11 @@
 import { attackedByWhite, attackedByBlack } from './attackedSquares.service.js'
+import { whiteIsInCheck, blackIsInCheck } from './isInCheck.service.js';
 
-export const dropPieces = function(startSquareId, targetSquareId, startingPieceColor){
+export const dropPieces = function(startSquareId, targetSquareId, move){
     let piece = document.getElementById(startSquareId).children[0];
     let targetSquare = document.getElementById(targetSquareId);
+
+    let lastMoveText = document.getElementById("lastMoveBox");
 
     if (piece.id.substring(6, 10) === "king" || piece.id.substring(6, 10) === "rook") {
         piece.setAttribute('hasMoved', 'true');
@@ -16,11 +19,16 @@ export const dropPieces = function(startSquareId, targetSquareId, startingPieceC
     }
     else {
         let targetPiece = targetSquare.children[0];
-            
-        targetSquare.appendChild(piece);
+
         targetSquare.removeChild(targetPiece);
+        targetSquare.appendChild(piece);
 
         attackedByWhite();
         attackedByBlack();
+
+        move = move.replace('-', 'x');
     }
+
+    if (whiteIsInCheck() === true || blackIsInCheck() === true) move += '+';
+    lastMoveText.innerText = move;
 }
